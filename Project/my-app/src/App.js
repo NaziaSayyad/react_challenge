5da23bc87ab59364295b13fb6bfd8c9adecc0cca
Jubershaikh2307 <66212074+Jubershaikh2307@users.noreply.github.com>
2023-03-30 02:36:38 +0530

    UI error changes

--- Project/my-app/src/App.js
+++ Project/my-app/src/App.js
@@ -1,18 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
 import Navbar from './Components/Navbar';
 import Sidebar from './Components/Sidebar';
 import { Box } from '@chakra-ui/react';
+import Footer from './Components/Footer';
 
 function App() {
-  const Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg23bqTJ8NSFQ6vjY81zv-2Av8fZY6Zls9gg&usqp=CAU"
 
   return (
     <div className="wrapper">
-      <Navbar
-        image={Image}
-      />
-      <Sidebar />
+      <Navbar/>
+      <Sidebar/>
+      <Footer/>
     </div>
   );
 }
